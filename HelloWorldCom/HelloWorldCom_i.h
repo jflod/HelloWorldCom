a60fc177ac392e7d5b3f0e6aa4046b1947394bbd
Jakob Flodman <jakob.flodman@payex.com>
2024-09-20 07:50:16 +0200

    Change toolset

---
+++ HelloWorldCom/HelloWorldCom_i.h
@@ -0,0 +1,462 @@
+
+
+/* this ALWAYS GENERATED file contains the definitions for the interfaces */
+
+
+ /* File created by MIDL compiler version 8.01.0622 */
+/* at Tue Jan 19 04:14:07 2038
+ */
+/* Compiler settings for HelloWorldCom.idl:
+    Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.01.0622 
+    protocol : dce , ms_ext, c_ext, robust
+    error checks: allocation ref bounds_check enum stub_data 
+    VC __declspec() decoration level: 
+         __declspec(uuid()), __declspec(selectany), __declspec(novtable)
+         DECLSPEC_UUID(), MIDL_INTERFACE()
+*/
+/* @@MIDL_FILE_HEADING(  ) */
+
+
+
+/* verify that the <rpcndr.h> version is high enough to compile this file*/
+#ifndef __REQUIRED_RPCNDR_H_VERSION__
+#define __REQUIRED_RPCNDR_H_VERSION__ 500
+#endif
+
+#include "rpc.h"
+#include "rpcndr.h"
+
+#ifndef __RPCNDR_H_VERSION__
+#error this stub requires an updated version of <rpcndr.h>
+#endif /* __RPCNDR_H_VERSION__ */
+
+#ifndef COM_NO_WINDOWS_H
+#include "windows.h"
+#include "ole2.h"
+#endif /*COM_NO_WINDOWS_H*/
+
+#ifndef __HelloWorldCom_i_h__
+#define __HelloWorldCom_i_h__
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+#pragma once
+#endif
+
+/* Forward Declarations */ 
+
+#ifndef __IComponentRegistrar_FWD_DEFINED__
+#define __IComponentRegistrar_FWD_DEFINED__
+typedef interface IComponentRegistrar IComponentRegistrar;
+
+#endif 	/* __IComponentRegistrar_FWD_DEFINED__ */
+
+
+#ifndef __IHelloWorld_FWD_DEFINED__
+#define __IHelloWorld_FWD_DEFINED__
+typedef interface IHelloWorld IHelloWorld;
+
+#endif 	/* __IHelloWorld_FWD_DEFINED__ */
+
+
+#ifndef __CompReg_FWD_DEFINED__
+#define __CompReg_FWD_DEFINED__
+
+#ifdef __cplusplus
+typedef class CompReg CompReg;
+#else
+typedef struct CompReg CompReg;
+#endif /* __cplusplus */
+
+#endif 	/* __CompReg_FWD_DEFINED__ */
+
+
+#ifndef __HelloWorld_FWD_DEFINED__
+#define __HelloWorld_FWD_DEFINED__
+
+#ifdef __cplusplus
+typedef class HelloWorld HelloWorld;
+#else
+typedef struct HelloWorld HelloWorld;
+#endif /* __cplusplus */
+
+#endif 	/* __HelloWorld_FWD_DEFINED__ */
+
+
+/* header files for imported files */
+#include "oaidl.h"
+#include "ocidl.h"
+#include "shobjidl.h"
+
+#ifdef __cplusplus
+extern "C"{
+#endif 
+
+
+#ifndef __IComponentRegistrar_INTERFACE_DEFINED__
+#define __IComponentRegistrar_INTERFACE_DEFINED__
+
+/* interface IComponentRegistrar */
+/* [unique][dual][uuid][object] */ 
+
+
+EXTERN_C const IID IID_IComponentRegistrar;
+
+#if defined(__cplusplus) && !defined(CINTERFACE)
+    
+    MIDL_INTERFACE("a817e7a2-43fa-11d0-9e44-00aa00b6770a")
+    IComponentRegistrar : public IDispatch
+    {
+    public:
+        virtual /* [id] */ HRESULT STDMETHODCALLTYPE Attach( 
+            /* [in] */ BSTR bstrPath) = 0;
+        
+        virtual /* [id] */ HRESULT STDMETHODCALLTYPE RegisterAll( void) = 0;
+        
+        virtual /* [id] */ HRESULT STDMETHODCALLTYPE UnregisterAll( void) = 0;
+        
+        virtual /* [id] */ HRESULT STDMETHODCALLTYPE GetComponents( 
+            /* [out] */ SAFEARRAY * *pbstrCLSIDs,
+            /* [out] */ SAFEARRAY * *pbstrDescriptions) = 0;
+        
+        virtual /* [id] */ HRESULT STDMETHODCALLTYPE RegisterComponent( 
+            /* [in] */ BSTR bstrCLSID) = 0;
+        
+        virtual /* [id] */ HRESULT STDMETHODCALLTYPE UnregisterComponent( 
+            /* [in] */ BSTR bstrCLSID) = 0;
+        
+    };
+    
+    
+#else 	/* C style interface */
+
+    typedef struct IComponentRegistrarVtbl
+    {
+        BEGIN_INTERFACE
+        
+        HRESULT ( STDMETHODCALLTYPE *QueryInterface )( 
+            IComponentRegistrar * This,
+            /* [in] */ REFIID riid,
+            /* [annotation][iid_is][out] */ 
+            _COM_Outptr_  void **ppvObject);
+        
+        ULONG ( STDMETHODCALLTYPE *AddRef )( 
+            IComponentRegistrar * This);
+        
+        ULONG ( STDMETHODCALLTYPE *Release )( 
+            IComponentRegistrar * This);
+        
+        HRESULT ( STDMETHODCALLTYPE *GetTypeInfoCount )( 
+            IComponentRegistrar * This,
+            /* [out] */ UINT *pctinfo);
+        
+        HRESULT ( STDMETHODCALLTYPE *GetTypeInfo )( 
+            IComponentRegistrar * This,
+            /* [in] */ UINT iTInfo,
+            /* [in] */ LCID lcid,
+            /* [out] */ ITypeInfo **ppTInfo);
+        
+        HRESULT ( STDMETHODCALLTYPE *GetIDsOfNames )( 
+            IComponentRegistrar * This,
+            /* [in] */ REFIID riid,
+            /* [size_is][in] */ LPOLESTR *rgszNames,
+            /* [range][in] */ UINT cNames,
+            /* [in] */ LCID lcid,
+            /* [size_is][out] */ DISPID *rgDispId);
+        
+        /* [local] */ HRESULT ( STDMETHODCALLTYPE *Invoke )( 
+            IComponentRegistrar * This,
+            /* [annotation][in] */ 
+            _In_  DISPID dispIdMember,
+            /* [annotation][in] */ 
+            _In_  REFIID riid,
+            /* [annotation][in] */ 
+            _In_  LCID lcid,
+            /* [annotation][in] */ 
+            _In_  WORD wFlags,
+            /* [annotation][out][in] */ 
+            _In_  DISPPARAMS *pDispParams,
+            /* [annotation][out] */ 
+            _Out_opt_  VARIANT *pVarResult,
+            /* [annotation][out] */ 
+            _Out_opt_  EXCEPINFO *pExcepInfo,
+            /* [annotation][out] */ 
+            _Out_opt_  UINT *puArgErr);
+        
+        /* [id] */ HRESULT ( STDMETHODCALLTYPE *Attach )( 
+            IComponentRegistrar * This,
+            /* [in] */ BSTR bstrPath);
+        
+        /* [id] */ HRESULT ( STDMETHODCALLTYPE *RegisterAll )( 
+            IComponentRegistrar * This);
+        
+        /* [id] */ HRESULT ( STDMETHODCALLTYPE *UnregisterAll )( 
+            IComponentRegistrar * This);
+        
+        /* [id] */ HRESULT ( STDMETHODCALLTYPE *GetComponents )( 
+            IComponentRegistrar * This,
+            /* [out] */ SAFEARRAY * *pbstrCLSIDs,
+            /* [out] */ SAFEARRAY * *pbstrDescriptions);
+        
+        /* [id] */ HRESULT ( STDMETHODCALLTYPE *RegisterComponent )( 
+            IComponentRegistrar * This,
+            /* [in] */ BSTR bstrCLSID);
+        
+        /* [id] */ HRESULT ( STDMETHODCALLTYPE *UnregisterComponent )( 
+            IComponentRegistrar * This,
+            /* [in] */ BSTR bstrCLSID);
+        
+        END_INTERFACE
+    } IComponentRegistrarVtbl;
+
+    interface IComponentRegistrar
+    {
+        CONST_VTBL struct IComponentRegistrarVtbl *lpVtbl;
+    };
+
+    
+
+#ifdef COBJMACROS
+
+
+#define IComponentRegistrar_QueryInterface(This,riid,ppvObject)	\
+    ( (This)->lpVtbl -> QueryInterface(This,riid,ppvObject) ) 
+
+#define IComponentRegistrar_AddRef(This)	\
+    ( (This)->lpVtbl -> AddRef(This) ) 
+
+#define IComponentRegistrar_Release(This)	\
+    ( (This)->lpVtbl -> Release(This) ) 
+
+
+#define IComponentRegistrar_GetTypeInfoCount(This,pctinfo)	\
+    ( (This)->lpVtbl -> GetTypeInfoCount(This,pctinfo) ) 
+
+#define IComponentRegistrar_GetTypeInfo(This,iTInfo,lcid,ppTInfo)	\
+    ( (This)->lpVtbl -> GetTypeInfo(This,iTInfo,lcid,ppTInfo) ) 
+
+#define IComponentRegistrar_GetIDsOfNames(This,riid,rgszNames,cNames,lcid,rgDispId)	\
+    ( (This)->lpVtbl -> GetIDsOfNames(This,riid,rgszNames,cNames,lcid,rgDispId) ) 
+
+#define IComponentRegistrar_Invoke(This,dispIdMember,riid,lcid,wFlags,pDispParams,pVarResult,pExcepInfo,puArgErr)	\
+    ( (This)->lpVtbl -> Invoke(This,dispIdMember,riid,lcid,wFlags,pDispParams,pVarResult,pExcepInfo,puArgErr) ) 
+
+
+#define IComponentRegistrar_Attach(This,bstrPath)	\
+    ( (This)->lpVtbl -> Attach(This,bstrPath) ) 
+
+#define IComponentRegistrar_RegisterAll(This)	\
+    ( (This)->lpVtbl -> RegisterAll(This) ) 
+
+#define IComponentRegistrar_UnregisterAll(This)	\
+    ( (This)->lpVtbl -> UnregisterAll(This) ) 
+
+#define IComponentRegistrar_GetComponents(This,pbstrCLSIDs,pbstrDescriptions)	\
+    ( (This)->lpVtbl -> GetComponents(This,pbstrCLSIDs,pbstrDescriptions) ) 
+
+#define IComponentRegistrar_RegisterComponent(This,bstrCLSID)	\
+    ( (This)->lpVtbl -> RegisterComponent(This,bstrCLSID) ) 
+
+#define IComponentRegistrar_UnregisterComponent(This,bstrCLSID)	\
+    ( (This)->lpVtbl -> UnregisterComponent(This,bstrCLSID) ) 
+
+#endif /* COBJMACROS */
+
+
+#endif 	/* C style interface */
+
+
+
+
+#endif 	/* __IComponentRegistrar_INTERFACE_DEFINED__ */
+
+
+#ifndef __IHelloWorld_INTERFACE_DEFINED__
+#define __IHelloWorld_INTERFACE_DEFINED__
+
+/* interface IHelloWorld */
+/* [unique][nonextensible][dual][uuid][object] */ 
+
+
+EXTERN_C const IID IID_IHelloWorld;
+
+#if defined(__cplusplus) && !defined(CINTERFACE)
+    
+    MIDL_INTERFACE("ad103ffc-d76c-4a69-ad9a-645dff55b240")
+    IHelloWorld : public IDispatch
+    {
+    public:
+        virtual /* [id] */ HRESULT STDMETHODCALLTYPE Start( 
+            /* [in] */ BSTR name,
+            /* [retval][out] */ BSTR *result) = 0;
+        
+    };
+    
+    
+#else 	/* C style interface */
+
+    typedef struct IHelloWorldVtbl
+    {
+        BEGIN_INTERFACE
+        
+        HRESULT ( STDMETHODCALLTYPE *QueryInterface )( 
+            IHelloWorld * This,
+            /* [in] */ REFIID riid,
+            /* [annotation][iid_is][out] */ 
+            _COM_Outptr_  void **ppvObject);
+        
+        ULONG ( STDMETHODCALLTYPE *AddRef )( 
+            IHelloWorld * This);
+        
+        ULONG ( STDMETHODCALLTYPE *Release )( 
+            IHelloWorld * This);
+        
+        HRESULT ( STDMETHODCALLTYPE *GetTypeInfoCount )( 
+            IHelloWorld * This,
+            /* [out] */ UINT *pctinfo);
+        
+        HRESULT ( STDMETHODCALLTYPE *GetTypeInfo )( 
+            IHelloWorld * This,
+            /* [in] */ UINT iTInfo,
+            /* [in] */ LCID lcid,
+            /* [out] */ ITypeInfo **ppTInfo);
+        
+        HRESULT ( STDMETHODCALLTYPE *GetIDsOfNames )( 
+            IHelloWorld * This,
+            /* [in] */ REFIID riid,
+            /* [size_is][in] */ LPOLESTR *rgszNames,
+            /* [range][in] */ UINT cNames,
+            /* [in] */ LCID lcid,
+            /* [size_is][out] */ DISPID *rgDispId);
+        
+        /* [local] */ HRESULT ( STDMETHODCALLTYPE *Invoke )( 
+            IHelloWorld * This,
+            /* [annotation][in] */ 
+            _In_  DISPID dispIdMember,
+            /* [annotation][in] */ 
+            _In_  REFIID riid,
+            /* [annotation][in] */ 
+            _In_  LCID lcid,
+            /* [annotation][in] */ 
+            _In_  WORD wFlags,
+            /* [annotation][out][in] */ 
+            _In_  DISPPARAMS *pDispParams,
+            /* [annotation][out] */ 
+            _Out_opt_  VARIANT *pVarResult,
+            /* [annotation][out] */ 
+            _Out_opt_  EXCEPINFO *pExcepInfo,
+            /* [annotation][out] */ 
+            _Out_opt_  UINT *puArgErr);
+        
+        /* [id] */ HRESULT ( STDMETHODCALLTYPE *Start )( 
+            IHelloWorld * This,
+            /* [in] */ BSTR name,
+            /* [retval][out] */ BSTR *result);
+        
+        END_INTERFACE
+    } IHelloWorldVtbl;
+
+    interface IHelloWorld
+    {
+        CONST_VTBL struct IHelloWorldVtbl *lpVtbl;
+    };
+
+    
+
+#ifdef COBJMACROS
+
+
+#define IHelloWorld_QueryInterface(This,riid,ppvObject)	\
+    ( (This)->lpVtbl -> QueryInterface(This,riid,ppvObject) ) 
+
+#define IHelloWorld_AddRef(This)	\
+    ( (This)->lpVtbl -> AddRef(This) ) 
+
+#define IHelloWorld_Release(This)	\
+    ( (This)->lpVtbl -> Release(This) ) 
+
+
+#define IHelloWorld_GetTypeInfoCount(This,pctinfo)	\
+    ( (This)->lpVtbl -> GetTypeInfoCount(This,pctinfo) ) 
+
+#define IHelloWorld_GetTypeInfo(This,iTInfo,lcid,ppTInfo)	\
+    ( (This)->lpVtbl -> GetTypeInfo(This,iTInfo,lcid,ppTInfo) ) 
+
+#define IHelloWorld_GetIDsOfNames(This,riid,rgszNames,cNames,lcid,rgDispId)	\
+    ( (This)->lpVtbl -> GetIDsOfNames(This,riid,rgszNames,cNames,lcid,rgDispId) ) 
+
+#define IHelloWorld_Invoke(This,dispIdMember,riid,lcid,wFlags,pDispParams,pVarResult,pExcepInfo,puArgErr)	\
+    ( (This)->lpVtbl -> Invoke(This,dispIdMember,riid,lcid,wFlags,pDispParams,pVarResult,pExcepInfo,puArgErr) ) 
+
+
+#define IHelloWorld_Start(This,name,result)	\
+    ( (This)->lpVtbl -> Start(This,name,result) ) 
+
+#endif /* COBJMACROS */
+
+
+#endif 	/* C style interface */
+
+
+
+
+#endif 	/* __IHelloWorld_INTERFACE_DEFINED__ */
+
+
+
+#ifndef __HelloWorldComLib_LIBRARY_DEFINED__
+#define __HelloWorldComLib_LIBRARY_DEFINED__
+
+/* library HelloWorldComLib */
+/* [custom][version][uuid] */ 
+
+
+EXTERN_C const IID LIBID_HelloWorldComLib;
+
+EXTERN_C const CLSID CLSID_CompReg;
+
+#ifdef __cplusplus
+
+class DECLSPEC_UUID("ffae472e-986e-4449-b730-35deb0b2c0f4")
+CompReg;
+#endif
+
+EXTERN_C const CLSID CLSID_HelloWorld;
+
+#ifdef __cplusplus
+
+class DECLSPEC_UUID("7139a489-66eb-4cf1-b1b9-62f3dbfb373e")
+HelloWorld;
+#endif
+#endif /* __HelloWorldComLib_LIBRARY_DEFINED__ */
+
+/* Additional Prototypes for ALL interfaces */
+
+unsigned long             __RPC_USER  BSTR_UserSize(     unsigned long *, unsigned long            , BSTR * ); 
+unsigned char * __RPC_USER  BSTR_UserMarshal(  unsigned long *, unsigned char *, BSTR * ); 
+unsigned char * __RPC_USER  BSTR_UserUnmarshal(unsigned long *, unsigned char *, BSTR * ); 
+void                      __RPC_USER  BSTR_UserFree(     unsigned long *, BSTR * ); 
+
+unsigned long             __RPC_USER  LPSAFEARRAY_UserSize(     unsigned long *, unsigned long            , LPSAFEARRAY * ); 
+unsigned char * __RPC_USER  LPSAFEARRAY_UserMarshal(  unsigned long *, unsigned char *, LPSAFEARRAY * ); 
+unsigned char * __RPC_USER  LPSAFEARRAY_UserUnmarshal(unsigned long *, unsigned char *, LPSAFEARRAY * ); 
+void                      __RPC_USER  LPSAFEARRAY_UserFree(     unsigned long *, LPSAFEARRAY * ); 
+
+unsigned long             __RPC_USER  BSTR_UserSize64(     unsigned long *, unsigned long            , BSTR * ); 
+unsigned char * __RPC_USER  BSTR_UserMarshal64(  unsigned long *, unsigned char *, BSTR * ); 
+unsigned char * __RPC_USER  BSTR_UserUnmarshal64(unsigned long *, unsigned char *, BSTR * ); 
+void                      __RPC_USER  BSTR_UserFree64(     unsigned long *, BSTR * ); 
+
+unsigned long             __RPC_USER  LPSAFEARRAY_UserSize64(     unsigned long *, unsigned long            , LPSAFEARRAY * ); 
+unsigned char * __RPC_USER  LPSAFEARRAY_UserMarshal64(  unsigned long *, unsigned char *, LPSAFEARRAY * ); 
+unsigned char * __RPC_USER  LPSAFEARRAY_UserUnmarshal64(unsigned long *, unsigned char *, LPSAFEARRAY * ); 
+void                      __RPC_USER  LPSAFEARRAY_UserFree64(     unsigned long *, LPSAFEARRAY * ); 
+
+/* end of Additional Prototypes */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+
